bd57734439f57ad6e3f3c03f6af2a3ddc02cf406
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-16 16:40:44 +0200

    #6: Added skelet of XSLT transformer.

---
+++ src/edeposit/amqp/ltp/xslt_transformer.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+
+# Variables ===================================================================
+
+
+
+# Functions & objects =========================================================
+def transform(marc_xml, template):
+    pass
