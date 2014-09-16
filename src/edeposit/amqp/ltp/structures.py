4f6636ae239dd6d5d363f88bb6c088924ee50530
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-16 14:17:27 +0200

    #2: Added basic specification of communication structures.

--- src/edeposit/amqp/ltp/structures.py
+++ src/edeposit/amqp/ltp/structures.py
@@ -8,14 +8,21 @@ by Bystroushaak (bystrousak@kitakitsune.org)
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from collections import namedtuple
 
 
 # Variables ===================================================================
 
 
 # Functions & objects =========================================================
+class ExportRequest():
+    def __init__(self, aleph_record, filename, b64_data):
+        self.aleph_record = aleph_record
+        self.filename = filename
+        self.b64_data = b64_data
 
 
-# Main program ================================================================
-if __name__ == '__main__':
-	pass
+class ExportResult():
+    def __init__(self, export_id, exported):
+        self.export_id = export_id
+        self.exported = exported
