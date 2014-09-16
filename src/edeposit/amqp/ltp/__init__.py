3ee52c28dad7f6c92e389281dde60f27a4ed96cd
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-16 14:33:26 +0200

    #2: Added reactToAMQPMessage() skelet into __init__.py.

--- src/edeposit/amqp/ltp/__init__.py
+++ src/edeposit/amqp/ltp/__init__.py
@@ -1,1 +1,36 @@
-__import__('pkg_resources').declare_namespace(__name__)
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import structures
+
+
+# Variables ===================================================================
+
+
+
+# Functions & objects =========================================================
+
+
+
+# Main program ================================================================
+def reactToAMQPMessage(message, UUID):
+    """
+    React to given (AMQP) message. `message` is expected to be
+    :py:func:`collections.namedtuple` structure from :mod:`.structures` filled
+    with all necessary data.
+
+    Args:
+        message (object): One of the request objects defined in
+                          :mod:`.structures`.
+        UUID (str): Unique ID of received message.
+
+    Returns:
+        object: Response class from :mod:`structures`.
+
+    Raises:
+        ValueError: if bad type of `message` structure is given.
+    """
+    pass
