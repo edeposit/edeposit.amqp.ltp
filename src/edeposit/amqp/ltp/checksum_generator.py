1cae7dbb28ba62c8db4f812ca7371d705f0bc375
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-16 16:40:25 +0200

    #8; Added skelet of checksum generator.

---
+++ src/edeposit/amqp/ltp/checksum_generator.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+
+
+
+# Variables ===================================================================
+
+
+
+# Functions & objects =========================================================
+def generate_checksums(directory):
+    pass
