11725b90ee4c0641ce7807eecfe026f8e13e1689
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-16 17:05:49 +0200

    #8: Added code for generating checksums.

--- src/edeposit/amqp/ltp/checksum_generator.py
+++ src/edeposit/amqp/ltp/checksum_generator.py
@@ -4,13 +4,49 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-
+import os
+import hashlib
 
 
 # Variables ===================================================================
-
+_BLACKLIST = {
+    "info.xml"
+}
 
 
 # Functions & objects =========================================================
-def generate_checksums(directory):
-    pass
+def _get_required_fn(fn, root_path):
+    replacer = "/" if root_path.endswith("/") else ""
+
+    return fn.replace(root_path, replacer, 1)
+
+
+def generate_checksums(directory, blacklist=_BLACKLIST):
+    if not os.path.exists(directory):
+        raise UserWarning("'%s' doesn't exists!" % directory)
+
+    hashes = []
+    for root, dirs, files in os.walk(directory):
+        for fn in files:
+            # skip files on blacklist
+            if fn in blacklist:
+                continue
+
+            fn = os.path.join(root, fn)
+
+            # compute hash of the file
+            with open(fn) as f:
+                fn_hash = hashlib.md5(f.read())
+
+            # append pair (hash, fixed_fn)
+            hashes.append(
+                (
+                    fn_hash.hexdigest(),
+                    _get_required_fn(fn, directory)
+                )
+            )
+
+    return hashes
+
+
+print generate_checksums("/home/bystrousak/.ssh")
