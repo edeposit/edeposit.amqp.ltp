264c9565b6e9252c68d4430fc8a7b539ffcf5760
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-17 16:43:31 +0200

    #6: Added basic sketch of XSLT transformer.

--- src/edeposit/amqp/ltp/xslt_transformer.py
+++ src/edeposit/amqp/ltp/xslt_transformer.py
@@ -4,13 +4,34 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-
+import os.path
+import StringIO
+import lxml.etree as ET
 
 
 # Variables ===================================================================
 
 
-
 # Functions & objects =========================================================
-def transform(marc_xml, template):
-    pass
+def _read_marcxml(marc_xml):  # TODO: konverze marc OAI na marc XML
+    file_obj = StringIO.StringIO(marc_xml)
+
+    return ET.parse(file_obj)
+
+
+def _read_template_file(template_fn):
+    if not os.path.exists(template_fn):
+        raise UserWarning("'%s' doesn't exists!" % template_fn)
+
+    return ET.parse(template_fn)
+
+
+def transform(marc_xml, template_fn):
+    transformer = ET.XSLT(
+        _read_template_file(template_fn)
+    )
+    newdom = transformer(
+        _read_marcxml(marc_xml)
+    )
+
+    return ET.tostring(newdom, pretty_print=True)
