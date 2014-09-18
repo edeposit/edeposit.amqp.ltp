d299cee11ef2acb36178681bc40dc79a2506f4e9
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-18 17:56:10 +0200

    #6: Added _add_namespace().

--- src/edeposit/amqp/ltp/xslt_transformer.py
+++ src/edeposit/amqp/ltp/xslt_transformer.py
@@ -9,11 +9,29 @@ import StringIO
 import lxml.etree as ET
 
 
+import dhtmlparser
+
+
 # Variables ===================================================================
 
 
 # Functions & objects =========================================================
+def _add_namespace(marc_xml):
+    dom = dhtmlparser.parseString(marc_xml)
+
+    for col in dom.find("collection"):
+        col.params["xmlns"] = "http://www.loc.gov/MARC21/slim"
+        col.params["xmlns:xsi"] = "http://www.w3.org/2001/XMLSchema-instance"
+        col.params["xsi:schemaLocation"] = "http://www.loc.gov/MARC21/slim " + \
+                  "http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"
+
+    dom.find("root")[0].params = {}
+
+    return str(dom)
+
+
 def _read_marcxml(marc_xml):  # TODO: konverze marc OAI na marc XML
+    marc_xml = _add_namespace(marc_xml)
     file_obj = StringIO.StringIO(marc_xml)
 
     return ET.parse(file_obj)
@@ -35,3 +53,5 @@ def transform(marc_xml, template_fn):
     )
 
     return ET.tostring(newdom, pretty_print=True)
+
+print transform(open("/home/bystrousak/Plocha/LTP/aleph_example.xml").read(), "xslt/MARC21slim2MODS3-4-NDK.xsl")
