703072636ae0c1771e3329f1036aa70da0d417d7
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-03 15:04:22 +0200

    #2: Added `book_id` attribute to ExportRequest.

--- src/edeposit/amqp/ltp/structures.py
+++ src/edeposit/amqp/ltp/structures.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-by Bystroushaak (bystrousak@kitakitsune.org)
-"""
 #
 # Interpreter version: python 2.7
 #
@@ -16,8 +12,9 @@ from collections import namedtuple
 
 # Functions & objects =========================================================
 class ExportRequest():
-    def __init__(self, aleph_record, filename, b64_data):
+    def __init__(self, aleph_record, book_id, filename, b64_data):
         self.aleph_record = aleph_record
+        self.book_id = book_id
         self.filename = filename
         self.b64_data = b64_data
 
