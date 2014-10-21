63d4d48a2e3fd774444e187fba58cfb8f95ec707
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-21 13:47:15 +0200

    Added transform_to_mods_multimonograph() and transform_to_mods_periodical().

--- src/edeposit/amqp/ltp/xslt_transformer.py
+++ src/edeposit/amqp/ltp/xslt_transformer.py
@@ -170,3 +170,43 @@ def transform_to_mods(marc_xml):
     mods_template = os.path.join(dirname, "xslt/MARC21slim2MODS3-4-NDK.xsl")
 
     return transform(marc_xml, mods_template)
+
+
+def transform_to_mods_multimonograph(marc_xml):
+    """
+    Convert `marc_xml` to multimonograph MODS data format.
+
+    Args:
+        marc_xml (str): Filename or XML string. Don't use ``\\n`` in case of
+                        filename.
+
+    Returns:
+        str: Transformed xml as string.
+    """
+    dirname = os.path.dirname(__file__)
+    mods_template = os.path.join(
+        dirname,
+        "xslt/MARC21toMultiMonographTitle.xsl"
+    )
+
+    return transform(marc_xml, mods_template)
+
+
+def transform_to_mods_periodical(marc_xml):
+    """
+    Convert `marc_xml` to periodical MODS data format.
+
+    Args:
+        marc_xml (str): Filename or XML string. Don't use ``\\n`` in case of
+                        filename.
+
+    Returns:
+        str: Transformed xml as string.
+    """
+    dirname = os.path.dirname(__file__)
+    mods_template = os.path.join(
+        dirname,
+        "xslt/MARC21toPeriodicalTitle.xsl"
+    )
+
+    return transform(marc_xml, mods_template)
