13d9eb6ca11b1adfef041ea264534c9ea23a4a7a
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-23 16:39:29 +0200

    #11, #9: Rewrite of the unittests for fn_composers.py.

--- src/edeposit/amqp/ltp/tests/unittests/test_init.py
+++ src/edeposit/amqp/ltp/tests/unittests/test_init.py
@@ -24,24 +24,6 @@ HASH_FILE = DIRNAME + "hashfile.md5"
 
 
 # Tests =======================================================================
-def test_get_suffix():
-    assert ltp._get_suffix("/home/xex/somefile.txt") == "txt"
-    assert ltp._get_suffix("somefile.txt") == "txt"
-    assert ltp._get_suffix("/somefile.txt") == "txt"
-    assert ltp._get_suffix("somefile") == "somefile"
-    assert ltp._get_suffix("/home/xex/somefile") == "somefile"
-
-
-def test_get_original_fn():
-    assert ltp._get_original_fn("111", "somebook.epub") == "oc_nk-edep-111.epub"
-    assert ltp._get_original_fn(111, "somebook.pdf") == "oc_nk-edep-111.pdf"
-
-
-def test_get_metadata_fn():
-    assert ltp._get_metadata_fn("111") == "meds_nk-edep-111.xml"
-    assert ltp._get_metadata_fn(111) == "meds_nk-edep-111.xml"
-
-
 def test_create_package_hierarchy():
     root_dir, original_dir, metadata_dir = ltp._create_package_hierarchy("/tmp")
 
