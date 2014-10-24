2a60bfe6f492cafff06f4325e026e4a62a4bc376
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-24 16:44:21 +0200

    #9: Added tests of the mods_postprocessor.py.

---
+++ src/edeposit/amqp/ltp/tests/unittests/test_mods_postprocessor.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from ltp import mods_postprocessor, xslt_transformer
+import test_xslt_transformer
+
+
+# Variables ===================================================================
+POSTPROCESSED_FN = test_xslt_transformer.DIRNAME + "postprocessed_mods.xml"
+
+
+# Tests =======================================================================
+def test_postprocess_mods():
+    result = xslt_transformer.transform_to_mods(test_xslt_transformer.OAI_FILENAME)
+
+    with open(test_xslt_transformer.TRANSFORMED_FN) as f:
+        assert result == f.read()
+
+    postprocessed = mods_postprocessor.postprocess_mods(result, "someid")
+
+    with open(POSTPROCESSED_FN) as f:
+        assert postprocessed == f.read()
