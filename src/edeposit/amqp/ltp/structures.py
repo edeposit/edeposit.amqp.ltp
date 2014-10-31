3e567842671b1e915cd87979e8877c4b36fc50e2
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-31 14:27:49 +0100

    #2: Added ExportTrackingRequest, TrackingStates, ExportTrackingResult.

--- src/edeposit/amqp/ltp/structures.py
+++ src/edeposit/amqp/ltp/structures.py
@@ -23,3 +23,20 @@ class ExportResult():
     def __init__(self, export_id, exported):
         self.export_id = export_id
         self.exported = exported
+
+
+class ExportTrackingRequest():
+    def __init__(self, book_id):
+        self.book_id = book_id
+
+
+class TrackingStates():
+    def __init__(self, exported, error):
+        self.exported = exported
+        self.error = error
+
+
+class ExportTrackingResult():
+    def __init__(self, book_id, state):
+        self.book_id = book_id
+        self.state = state
