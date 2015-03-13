e5f28e6cd1b288e757882f0f2249b7091d7a8d79
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-13 13:37:08 +0100

    Modularization; info_composer.py created from ltp.py.

--- src/edeposit/amqp/ltp/ltp.py
+++ src/edeposit/amqp/ltp/ltp.py
@@ -6,22 +6,16 @@
 # Imports =====================================================================
 import os
 import uuid
-import time
 import shutil
 import base64
 import os.path
-import hashlib
-from collections import OrderedDict
-
-import xmltodict
-from edeposit.amqp.aleph import marcxml
 
 import settings
 
 import fn_composers
+import info_composer
 import checksum_generator
 from xslt_transformer import transform_to_mods
-from mods_postprocessor.shared_funcs import remove_hairs
 
 
 # Functions & objects =========================================================
@@ -79,227 +73,6 @@ def _create_package_hierarchy(prefix=settings.TEMP_DIR):
     return root_dir, original_dir, metadata_dir
 
 
-def _get_localized_fn(path, root_dir):
-    """
-    Return absolute `path` relative to `root_dir`.
-
-    When `path` == ``/home/xex/somefile.txt`` and `root_dir` == ``/home``,
-    returned path will be ``/xex/somefile.txt``.
-
-    Args:
-        path (str): Absolute path beginning in `root_dir`.
-        root_dir (str): Absolute path containing `path` argument.
-
-    Returns:
-        str: Local `path` when `root_dir` is considered as root of FS.
-    """
-    local_fn = path
-    if path.startswith(root_dir):
-        local_fn = path.replace(root_dir, "", 1)
-
-    if not local_fn.startswith("/"):
-        return "/" + local_fn
-
-    return local_fn
-
-
-def _path_to_id(path):
-    """
-    Name of the root directory is used as ``<packageid>`` in ``info.xml``.
-
-    This function makes sure, that :func:`os.path.basename` doesn't return
-    blank string in case that there is `/` at the end of the `path`.
-
-    Args:
-        path (str): Path to the root directory.
-
-    Returns:
-        str: Basename of the `path`.
-    """
-    if path.endswith("/"):
-        path = path[:-1]
-
-    return os.path.basename(path)
-
-
-def _calc_dir_size(path):
-    """
-    Calculate size of all files in `path`.
-
-    Args:
-        path (str): Path to the directory.
-
-    Returns:
-        int: Size of the directory in bytes.
-    """
-    dir_size = 0
-    for (root, dirs, files) in os.walk(path):
-        for fn in files:
-            full_fn = os.path.join(root, fn)
-            dir_size += os.path.getsize(full_fn)
-
-    return dir_size
-
-
-def _add_order(inp_dict):
-    """
-    Add order to unordered dict.
-
-    Order is taken from *priority table*, which is just something I did to
-    make outputs from `xmltodict` look like examples in specification.
-
-    Args:
-        inp_dict (dict): Unordered dictionary.
-
-    Returns:
-        OrderedDict: Dictionary ordered by *priority table*.
-    """
-    out = OrderedDict()
-
-    priority_table = [
-        "created",
-        "metadataversion",
-        "packageid",
-        "mainmets",
-        "titleid",
-        "collection",
-        "institution",
-        "creator",
-        "size",
-        "itemlist",
-        "checksum"
-    ]
-    priority_table = dict(  # construct dict keys -> {key: order}
-        map(
-            lambda (cnt, key): (key, cnt),
-            enumerate(priority_table)
-        )
-    )
-
-    sorted_keys = sorted(
-        inp_dict.keys(),
-        key=lambda x: priority_table.get(x, x)
-    )
-    for key in sorted_keys:
-        out[key] = inp_dict[key]
-
-    return out
-
-
-def _compose_info(root_dir, files, hash_fn, aleph_record):
-    """
-    Compose `info` XML file.
-
-    Info example::
-
-        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-        <info>
-            <created>2014-07-31T10:58:53</created>
-            <metadataversion>1.0</metadataversion>
-            <packageid>c88f5a50-7b34-11e2-b930-005056827e51</packageid>
-            <mainmets>mets.xml</mainmets>
-            <titleid type="ccnb">cnb001852189</titleid>
-            <titleid type="isbn">978-80-85979-89-6</titleid>
-            <collection>edeposit</collection>
-            <institution>nakladatelství Altar</institution>
-            <creator>ABA001</creator>
-            <size>1530226</size>
-            <itemlist itemtotal="1">
-                <item>\data\Denik_zajatce_Sramek_CZ_v30f-font.epub</item>
-            </itemlist>
-            <checksum type="MD5" checksum="ce076548eaade33888005de5d4634a0d">
-                \MD5.md5
-            </checksum>
-        </info>
-
-    Args:
-        root_dir (str): Absolute path to the root directory.
-        original_fn (str): Absolute path to the ebook file.
-        metadata_fn (str): Absolute path to the metadata file.
-        hash_fn (str): Absolute path to the MD5 file.
-        aleph_record (str): String with Aleph record with metadata.
-
-    Returns:
-        str: XML string.
-    """
-    # compute hash for hashfile
-    with open(hash_fn) as f:
-        hash_file_md5 = hashlib.md5(f.read()).hexdigest()
-
-    document = {
-        "info": {
-            "created": time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()),
-            "metadataversion": "1.0",
-            "packageid": _path_to_id(root_dir),
-
-            # not used in SIP
-            # "mainmets": _get_localized_fn(metadata_fn, root_dir),
-
-            "itemlist": {
-                "@itemtotal": "2",
-                "item": map(
-                    lambda x: _get_localized_fn(x, root_dir),
-                    files
-                )
-            },
-            "checksum": {
-                "@type": "MD5",
-                "@checksum": hash_file_md5,
-                "#text": _get_localized_fn(hash_fn, root_dir)
-            },
-            "collection": "edeposit",
-            "size": _calc_dir_size(root_dir) / 1024,  # size in kiB
-        }
-    }
-
-    # get informations from MARC record
-    record = marcxml.MARCXMLRecord(aleph_record)
-
-    # get publisher info
-    publisher = unicode(record.getPublisher(), "utf-8")
-    if record.getPublisher(None):
-        document["info"]["institution"] = remove_hairs(publisher)
-
-    # get <creator> info
-    creator = record.getDataRecords("910", "a", False)
-    alt_creator = record.getDataRecords("040", "d", False)
-    document["info"]["creator"] = creator[0] if creator else alt_creator[-1]
-
-    # collect informations for <titleid> tags
-    isbns = record.getISBNs()
-
-    ccnb = record.getDataRecords("015", "a", False)
-    ccnb = ccnb[0] if ccnb else None
-
-    if any([isbns, ccnb]):  # TODO: issn
-        document["info"]["titleid"] = []
-
-    for isbn in isbns:
-        document["info"]["titleid"].append({
-            "@type": "isbn",
-            "#text": isbn
-        })
-
-    if ccnb:
-        document["info"]["titleid"].append({
-            "@type": "ccnb",
-            "#text": ccnb
-        })
-
-    # TODO: later
-    # if issn:
-    #     document["info"]["titleid"].append({
-    #         "@type": "issn",
-    #         "#text": issn
-    #     })
-
-    document["info"] = _add_order(document["info"])
-    xml_document = xmltodict.unparse(document, pretty=True)
-
-    # return xml_document.replace("<?xml ", '<?xml standalone="yes" ')
-    return xml_document.encode("utf-8")
-
-
 def create_ltp_package(aleph_record, book_id, ebook_fn, b64_data):
     """
     Create LTP package as it is specified in specification v1.0 as I understand
@@ -316,7 +89,7 @@ def create_ltp_package(aleph_record, book_id, ebook_fn, b64_data):
     """
     root_dir, orig_dir, meta_dir = _create_package_hierarchy()
 
-    book_id = _path_to_id(root_dir)
+    book_id = info_composer._path_to_id(root_dir)
 
     # create original file
     original_fn = os.path.join(
@@ -352,7 +125,7 @@ def create_ltp_package(aleph_record, book_id, ebook_fn, b64_data):
     info_fn = os.path.join(root_dir, fn_composers.info_fn(book_id))
     with open(info_fn, "w") as f:
         f.write(
-            _compose_info(
+            info_composer.compose_info(
                 root_dir=root_dir,
                 files=[original_fn] + metadata_filenames,
                 hash_fn=md5_fn,
